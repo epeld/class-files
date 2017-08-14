90a7ca94cf87d64c4e5f70feba6b668be43d13d4
Erik Peldan <erik.peldan@gmail.com>
2017-08-14 20:41:42 +0200

    Add another sample java class

---
+++ Sample.java
@@ -0,0 +1,10 @@
+
+class Sample {
+    public static final int MY_INT = 33;
+    public static final float MY_FLOAT = 44;
+    public static volatile String MY_String = "FIFTY FIVE";
+
+    public boolean getIt() {
+        return true;
+    }
+}
