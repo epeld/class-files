55ac4c91b16d162224cb052292e556dceb6d0366
Erik Peldan <erik.peldan@gmail.com>
2017-08-20 22:02:21 +0200

    Add a class list to the web server

--- server.lisp
+++ server.lisp
@@ -38,20 +38,36 @@
     (- (length known-classes)
        len)))
 
+
 (hunchentoot:define-easy-handler (say-yo :uri "/") (jar-file)
-  
-   
+  (let ((num-known (length known-classes)))
+    (cl-who:with-html-output-to-string (s)
+      (:h1 "Hello")
+      (unless (zerop num-known)
+        (htm (:div (str (format nil "~a known java classes. " num-known))
+                   (:a :href "/classes" "View the Class List!"))))
+      (:div )
+      (:div "To add classes, please POST a jar file below and you will receive info")
+      (:hr)
+      (when jar-file
+        (htm (:p (str (format nil "Jar file ~s loaded (~a new classes)~%" (second jar-file) (load-jar (first jar-file)))))
+             (:hr)))
+      (:form :name "file-upload"
+             :method "post"
+             :enctype "multipart/form-data"
+             (:label :for "jar-file" "Jar File:")
+             (:input :type "file" :name "jar-file" :size "1000000")
+             (:input :type "submit" :name "upload" :value "Upload")))))
+
+
+(hunchentoot:define-easy-handler (say-yo :uri "/classes") ()
   (cl-who:with-html-output-to-string (s)
-    (:h1 "Hello")
-    (:div "Jar file is" (str jar-file))
-    (:div "Please POST a jar file below and you will receive info")
-    (:hr)
-    (when jar-file
-      (htm (:p "Jar file ~s loaded (~a new classes)~%" (second jar-file) (load-jar (first jar-file)))
-           (:hr)))
-    (:form :name "file-upload"
-           :method "post"
-           :enctype "multipart/form-data"
-           (:label :for "jar-file" "Jar File:")
-           (:input :type "file" :name "jar-file" :size "1000000")
-           (:input :type "submit" :name "upload" :value "Upload"))))
+    (:h1 "List of Known Classes")
+    (:div "You can add more classes by uploading jar-files on " (:a :href "/" "the index-page"))
+    (:ul
+     (loop for c in known-classes do
+          (htm (:li (:a :href (str (format nil "/~a" (java-class-name c))) (str (java-class-name c)))))))))
+
+
+
+;; TODO write a handler for viewing an individual class!
