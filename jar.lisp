c82dda7bf8fa9e375227fa3035064100ba482a1e
Erik Peldan <erik.peldan@gmail.com>
2017-08-20 21:53:56 +0200

    Automatically filter out 'system-classes' when loading jar

--- jar.lisp
+++ jar.lisp
@@ -6,14 +6,25 @@
   (search suffix string :from-end t))
 
 
-(defun read-jar (file-path)
+(defun system-class-p (string)
+  "Predicate for filtering out 'system'-like classes, e.g junit etc so that only user defined classes remain"
+  (or (search "org/junit" string)
+      (search "org/hamcrest" string)
+      (search "junit/" string)))
+
+(defun read-jar (file-path &optional (user t))
   (zip:with-zipfile (z file-path)
     (let (classes)
       (zip:do-zipfile-entries (name entry z)
         (when (suffixp ".class" name)
           (let ((stream (flexi-streams:make-in-memory-input-stream (zip:zipfile-entry-contents entry))))
             (push (parse-class-stream stream) classes))))
-      classes)))
+
+      ;; Filter if necessary
+      (if user
+          (let ((user-classes (remove-if #'system-class-p classes :key #'java-class-name)))
+            (values user-classes (- (length classes) (length user-classes))))
+          (values classes 0)))))
 
 
 
