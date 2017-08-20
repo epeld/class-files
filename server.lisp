b7112efa16ed36760e266630940431d58e6fc364
Erik Peldan <erik.peldan@gmail.com>
2017-08-20 21:54:19 +0200

    Server merges all jars it receives

--- server.lisp
+++ server.lisp
@@ -27,15 +27,28 @@
 (defvar known-classes nil
   "List of known classes")
 
+
+(defun load-jar (path)
+  "Load a new jar file with all contained classes into the list of known classes. Returns the number of new classes added"
+  (let ((len (length known-classes)))
+    (setf known-classes (union (read-jar path)
+                               known-classes
+                               :key #'java-class-name
+                               :test #'string=))
+    (- (length known-classes)
+       len)))
+
 (hunchentoot:define-easy-handler (say-yo :uri "/") (jar-file)
-  (when jar-file
-    (setf known-classes (read-jar (first jar-file))))
   
-  (format t "Jar file: ~a ~a~%" jar-file (type-of jar-file))
+   
   (cl-who:with-html-output-to-string (s)
     (:h1 "Hello")
     (:div "Jar file is" (str jar-file))
     (:div "Please POST a jar file below and you will receive info")
+    (:hr)
+    (when jar-file
+      (htm (:p "Jar file ~s loaded (~a new classes)~%" (second jar-file) (load-jar (first jar-file)))
+           (:hr)))
     (:form :name "file-upload"
            :method "post"
            :enctype "multipart/form-data"
